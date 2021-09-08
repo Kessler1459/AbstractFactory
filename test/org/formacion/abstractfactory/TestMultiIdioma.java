f3f9abf116b787e963434c139d72326c8b5ce3f0
Kessler1459 <kessler1459@gmail.com>
2021-09-08 14:56:45 -0300

    correccion

--- test/org/formacion/abstractfactory/TestMultiIdioma.java
+++ test/org/formacion/abstractfactory/TestMultiIdioma.java
@@ -2,7 +2,6 @@ package org.formacion.abstractfactory;
 
 import static org.junit.Assert.*;
 
-import org.formacion.abstractfactory.factory.EnFactory;
 import org.formacion.abstractfactory.factory.FactoryMaker;
 import org.formacion.abstractfactory.factory.MessageFactory;
 import org.junit.Test;
@@ -11,48 +10,31 @@ public class TestMultiIdioma {
 
 	@Test
 	public void test_es() {
-		Preguntas preguntas = new PreguntasEs();
-		
+		MessageFactory factory= FactoryMaker.makeFactory(FactoryMaker.Language.ES);
+		Preguntas preguntas=factory.createPreguntas();
+
 		assertEquals("¿qué hora es?", preguntas.preguntaHora() );
 		assertEquals("¿qué tiempo hace?", preguntas.preguntaTiempo() );
-		
-		Saludos saludos = new SaludosEs();
-		
+
+		Saludos saludos=factory.createSaludos();
+
 		assertEquals("buenos días", saludos.buenosDias());
 		assertEquals("buenas tardes", saludos.buenasTardes());
 	}
 	
 	@Test
 	public void test_en() {
-		Preguntas preguntas = new PreguntasEn();
-		
+		MessageFactory factory= FactoryMaker.makeFactory(FactoryMaker.Language.EN);
+		Preguntas preguntas=factory.createPreguntas();
+
 		assertEquals("what time is it?", preguntas.preguntaHora() );
 		assertEquals("how is the weather?", preguntas.preguntaTiempo() );
-		
-		Saludos saludos = new SaludosEn();
+
+		Saludos saludos=factory.createSaludos();
 		
 		assertEquals("good morning", saludos.buenosDias());
 		assertEquals("good afternoon", saludos.buenasTardes());
 	}
 
-	@Test
-	public void test(){
-		MessageFactory enFactory= FactoryMaker.makeFactory(FactoryMaker.Language.EN);
-		MessageFactory esFactory= FactoryMaker.makeFactory(FactoryMaker.Language.ES);
-
-		Preguntas enPreguntas=enFactory.createPreguntas();
-		Saludos enSaludos=enFactory.createSaludos();
-		assertEquals("what time is it?", enPreguntas.preguntaHora());
-		assertEquals("how is the weather?", enPreguntas.preguntaTiempo());
-		assertEquals("good morning", enSaludos.buenosDias());
-		assertEquals("good afternoon", enSaludos.buenasTardes());
-
-		Preguntas esPreguntas=esFactory.createPreguntas();
-		Saludos esSaludos=esFactory.createSaludos();
-		assertEquals("¿qué hora es?", esPreguntas.preguntaHora() );
-		assertEquals("¿qué tiempo hace?", esPreguntas.preguntaTiempo() );
-		assertEquals("buenos días", esSaludos.buenosDias());
-		assertEquals("buenas tardes", esSaludos.buenasTardes());
-	}
 
 }
